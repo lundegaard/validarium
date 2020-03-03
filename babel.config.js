e5f78015287f18144d9831e95b1510c797aad478
Tomáš Konrády <tomaskonrady@gmail.com>
2020-03-03 16:06:35 +0100

    fix: removing Ramda from es and cjs bundles

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,7 @@
 module.exports = api => {
 	api.cache.using(() => process.env.NODE_ENV);
 
-	return api.env('test')
-		? {
-				presets: ['babel-preset-react-union'],
-		  }
-		: {
-				presets: ['babel-preset-react-union'],
-				plugins: [
-					[
-						'babel-plugin-ramda',
-						{
-							useES: true,
-						},
-					],
-				],
-		  };
+	return {
+		presets: ['babel-preset-react-union'],
+	};
 };
